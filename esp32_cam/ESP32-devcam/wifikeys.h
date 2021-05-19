0db1df413dd091ac8e9065a85c03fdfe926ce5bd
Alexey <31387726+Calliska@users.noreply.github.com>
2021-05-19 21:24:46 +0300

    Add files via upload

---
+++ esp32_cam/ESP32-devcam/wifikeys.h
@@ -0,0 +1,3 @@
+// copy this file to wifikeys.h and edit
+const char *ssid =     "";         // Put your SSID here
+const char *password = "";     // Put your PASSWORD here
---
+++ esp32_cam/Micro-RTSP_ID6071/examples/ESP32-devcam/wifikeys.h
@@ -0,0 +1,3 @@
+// copy this file to wifikeys.h and edit
+const char *ssid =     "Panda";         // Put your SSID here
+const char *password = "n6kLfPJS";     // Put your PASSWORD here
